c833dd6b116b111e394cbae17c1fe4720c2c8605
jayprakash622 <jayprakashkumar87578@gmail.com>
2026-01-03 14:52:21 +0530

    Update _08_second_largest.java

--- Array/_08_second_largest.java
+++ Array/_08_second_largest.java
@@ -3,25 +3,25 @@ public class _08_second_largest {
 
     public static int secondLargest(int num[]) {
         int largest = Integer.MIN_VALUE;
-        int result = 0;
+        int secondlargest = Integer.MIN_VALUE;
 
         for(int i = 0 ; i < num.length ; i++){
-          if(num[i] < num[largest]){
-            if(result == 0){
-                result = i;
+          if(num[i] > largest){
+              secondlargest =  largest
+                largest =num[i] ;
             }
-            else if(num[i] >num[result]){
-                result =i;
+            else if(num[i] >secondlargest && num[i] != largest){ 
+               secondlargest =  num[i] ;
             }
             
           }
           
         }
-        return result;
+        return secondlargest;
         
     }
     public static void main(String[] args) {
         int num[] = {2,4,6,8,7,5};
-        System.out.println("Second lsrgest number is :" + secondLargest(num));
+        System.out.println("Second largest number is :" + secondLargest(num));
     }
 }
